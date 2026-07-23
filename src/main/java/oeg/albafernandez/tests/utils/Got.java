faae4fed935550a7eadcee35212ee24261970383
Raúl García Castro <rgarcia@fi.upm.es>
2026-07-23 12:27:36 +0200

    Optimized string appends.

--- src/main/java/oeg/albafernandez/tests/utils/Got.java
+++ src/main/java/oeg/albafernandez/tests/utils/Got.java
@@ -1,6 +1,5 @@
 package oeg.albafernandez.tests.utils;
 
-import org.json.JSONArray;
 import org.semanticweb.owlapi.model.IRI;
 
 import java.util.*;
@@ -27,33 +26,32 @@ public class Got {
     }
 
     public String generateTable(HashMap<String, IRI> terms, String type) {
-        String tableLocal = "";
+        StringBuilder tableLocal = new StringBuilder();
         List<String> sortedByKey = new ArrayList<>(terms.keySet());
         Collections.sort(sortedByKey);
         for (String key : sortedByKey) {
-            tableLocal += "\t\t\t\t<tr class = \"got\">\n";
-            tableLocal += "\t\t\t\t\t<td  class=\"term got\" class=\"tg-031e\">" + key + "</td>\n";
-            tableLocal += "\t\t\t\t\t<td class=\"uri got tg-031e \">" + terms.get(key) + "</td>\n";
-            tableLocal += "\t\t\t\t\t<td class=\"type got tg-031e \">" + type + "</td>\n";
-            tableLocal += "\t\t\t\t\t<td class=\"tg-031e\"> <button  id=\"bEdit\" type=\"button\" class=\"btn btn-sm btn-default\" onclick=\"rowEdit(this);\"><span class=\"glyphicon glyphicon-pencil\"></span>EDIT TERM</button> </td>\n";
-            tableLocal += "\t\t\t\t\t<td class=\"tg-031e\">  <button  id=\"bRemove\" type=\"button\" class=\"btn btn-sm btn-default\" onclick=\"rowSave(this);\">SAVE </button> </td>\n";
-            tableLocal += "\t\t\t\t</tr>\n";
+            tableLocal.append("\t\t\t\t<tr class = \"got\">\n");
+            tableLocal.append("\t\t\t\t\t<td  class=\"term got\" class=\"tg-031e\">").append(key).append("</td>\n");
+            tableLocal.append("\t\t\t\t\t<td class=\"uri got tg-031e \">").append(terms.get(key)).append("</td>\n");
+            tableLocal.append("\t\t\t\t\t<td class=\"type got tg-031e \">").append(type).append("</td>\n");
+            tableLocal.append("\t\t\t\t\t<td class=\"tg-031e\"> <button  id=\"bEdit\" type=\"button\" class=\"btn btn-sm btn-default\" onclick=\"rowEdit(this);\"><span class=\"glyphicon glyphicon-pencil\"></span>EDIT TERM</button> </td>\n");
+            tableLocal.append("\t\t\t\t\t<td class=\"tg-031e\">  <button  id=\"bRemove\" type=\"button\" class=\"btn btn-sm btn-default\" onclick=\"rowSave(this);\">SAVE </button> </td>\n");
+            tableLocal.append("\t\t\t\t</tr>\n");
         }
-        return tableLocal;
+        return tableLocal.toString();
     }
 
     public String generateReport(String key, HashMap<String, IRI> classes, Map<String, IRI> op, Map<String, IRI> dp, Map<String, IRI> individuals) {
-        String report = "";
+        StringBuilder report = new StringBuilder();
 
-        report += TextConstants.headclasseswithkey(key);
+        report.append(TextConstants.headclasseswithkey(key));
+        report.append(generateTable(classes, "Class"));
+        report.append(generateTable((HashMap<String, IRI>) op, "Object property"));
+        report.append(generateTable((HashMap<String, IRI>) dp, "Datatype property"));
+        report.append(generateTable((HashMap<String, IRI>) individuals, "Individual"));
+        report.append(TextConstants.endTables);
 
-        report += generateTable(classes, "Class");
-        report += generateTable((HashMap<String, IRI>) op, "Object property");
-        report += generateTable((HashMap<String, IRI>) dp, "Datatype property");
-        report += generateTable((HashMap<String, IRI>) individuals, "Individual");
-        report += TextConstants.endTables;
-        return report;
-
+        return report.toString();
     }
 
 
